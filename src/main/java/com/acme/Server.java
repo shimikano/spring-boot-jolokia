4475745ac5278ff0292b71f1666eb009037a7cb2
shimikano <shimikano@users.noreply.github.com>
2014-05-14 16:13:21 +0200

    Simplified launching.

--- src/main/java/com/acme/Server.java
+++ src/main/java/com/acme/Server.java
@@ -3,15 +3,11 @@ package com.acme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+@EnableAutoConfiguration
 public final class Server {
 
   public static void main(String[] args) {
-    SpringApplication.run(ServerConfig.class, args);
+    SpringApplication.run(Server.class, args);
   }
-  
-  @EnableAutoConfiguration
-  public static class ServerConfig {
-    // empty
-  }
-  
+
 }
